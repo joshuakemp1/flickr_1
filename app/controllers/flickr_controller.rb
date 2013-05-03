3e8f06995a538c246d8c4b64fe5d3b77dca9c241
joshua kemp <joshuakemp85@gmail.com>
2013-05-02 23:44:17 -0400

    adding (1) end to the flickr controller, to try and get rid of heroku logs error.

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,30 +1,3 @@
-class FlickrController < ApplicationController
-  #require 'active_support'
-  require 'active_support/core_ext/hash'
-  require 'net/http'
-  require 'json'
-  FLICKR_USER_ID = ''
-  res = Net::HTTP.get('api.flickr.com',"/services/feeds/photos_public.gne?id=#{FLICKR_USER_ID}")
-  res_hash = Hash.from_xml(res)
-  res_hash["feed"]["entry"].each do |image|
-    puts image["link"][1]["href"]
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 require 'flickraw'
 
 FlickRaw.api_key="20b3d4317f2569c5f0a49040f229cf6f"
@@ -36,7 +9,7 @@ getPhotos = flickr.photosets.getPhotos(:photo_set => "72157633406435974")
 FlickRaw.url_photosets(getPhotos) => "http://www.flickr.com/photos/95255902@N05/sets/72157633406435974/"
 
 
-
+end
 
 
 
