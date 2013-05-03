815b6aec0403d12df07fecc56412a991f04b965e
joshua kemp <joshuakemp85@gmail.com>
2013-05-03 19:20:36 -0400

    deleting a hash in the flickr controller

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -6,7 +6,7 @@ FlickRaw.shared_secret="98e7c8faff9dbd99"
 FLICKR_USER_ID = '95255902@N05' 
 
 getPhotos = flickr.photosets.getPhotos(:photo_set => "72157633406435974")
-FlickRaw.url_photosets(getPhotos) # => "http://www.flickr.com/photos/95255902@N05/sets/72157633406435974/"
+FlickRaw.url_photosets(getPhotos)  => "http://www.flickr.com/photos/95255902@N05/sets/72157633406435974/"
 
 
 
