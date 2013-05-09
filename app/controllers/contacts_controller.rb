6df73a5f185c89c3705286a6f2135ea25b87e729
joshua kemp <joshuakemp85@gmail.com>
2013-05-08 23:29:12 -0400

    prettying up the layout some more

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,4 @@
 class ContactsController < ApplicationController
-  # GET /contacts
-  # GET /contacts.json
-  def index
-    @contacts = Contact.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @contacts }
-    end
-  end
-
   # GET /contacts/1
   # GET /contacts/1.json
   def show
@@ -32,10 +21,8 @@ class ContactsController < ApplicationController
     end
   end
 
-  # GET /contacts/1/edit
-  def edit
-    @contact = Contact.find(params[:id])
-  end
+  
+ 
 
   # POST /contacts
   # POST /contacts.json
@@ -44,7 +31,7 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.html { redirect_to @contact, notice: 'Wellspring service request sent! We will be in touch soon!' }
         format.json { render json: @contact, status: :created, location: @contact }
       else
         format.html { render action: "new" }
@@ -52,32 +39,7 @@ class ContactsController < ApplicationController
       end
     end
   end
+ 
 
-  # PUT /contacts/1
-  # PUT /contacts/1.json
-  def update
-    @contact = Contact.find(params[:id])
-
-    respond_to do |format|
-      if @contact.update_attributes(params[:contact])
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /contacts/1
-  # DELETE /contacts/1.json
-  def destroy
-    @contact = Contact.find(params[:id])
-    @contact.destroy
-
-    respond_to do |format|
-      format.html { redirect_to contacts_url }
-      format.json { head :no_content }
-    end
-  end
+  
 end
