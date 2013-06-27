6b3bf5c0c883f0da4ad6fc9d186e4418cad462c8
Richard Lewis <richard.lewis@tracesmart.co.uk>
2013-06-27 08:43:27 +0100

    removed flikr config out of controller into a module, added ENV file to store credentials for security, changed db to mysql, added .rvmrc file for gemset, stored secret token as ENV...again for security

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,49 +1,23 @@
-require 'flickraw'
-
 class PagesController < ApplicationController
-  FLICKR_USER_ID='95255902@N05' 
-  def home
-  end
 
-  def about
-  end
+include Photo::PhotoRetriever
+ 
+def home
 
-  def snagging_images
-      FlickRaw.api_key="20b3d4317f2569c5f0a49040f229cf6f"
-      FlickRaw.shared_secret="98e7c8faff9dbd99"
-      FLICKR_USER_ID
+end
 
-      i = 0
-      @photo = []
-      list = flickr.photos.search(:user_id => '95255902@N05')
-      @length = list.length
-    until i >=  @length do
-      info = flickr.photos.getInfo(:photo_id => list[i].id)
-      @photo[i] = FlickRaw.url_b(info)
-      i +=1
-    end
+def about
 
+end
 
-        # This one works but only for 1 photo :(
-        # info = flickr.photos.getInfo(:photo_id => "8706241131")
-        # @photo = FlickRaw.url_b(info)
+  
+def services
+  
+end
 
-        # info = flickr.photos.getInfo(:photo_id => "8702627447")
-        # @photo = FlickRaw.url_photopage(info) # => "http://www.flickr.com/photos/41650587@N02/3839885270"
-
-        # info = flickr.photos.getInfo(:photo_id => "8702627447")
-        # @photo = FlickRaw.url_photostream(info)
-
-         #getPhotos = flickr.photosets.getPhotos(:photo_set => "72157633406435974")
-        # FlickRaw.url_photosets(getPhotos)  # => "http://www.flickr.com/photos/95255902@N05/sets/72157633406435974/"
-    end
-
-  def services
-  end
-
-  def portfolio
-    snagging_images
-  end
+def portfolio
+  snagging_images
+end
 
   
 end
