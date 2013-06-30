3662ac8e8e5d1144676c46076a534be75b8872e0
richard <richard@richard-Inspiron-5523.(none)>
2013-06-30 21:09:11 +0100

    mailer config for produciton

--- Gemfile
+++ Gemfile
@@ -22,3 +22,4 @@ gem 'heroku'
 
 
 
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,17 @@ Practice1::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'} 
 
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.sendgrid.net",
   port: 587,
   domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV['EMAIL_USERNAME'],
-  password: ENV['EMAIL_PASSWORD']
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD']
 }
 
 end
