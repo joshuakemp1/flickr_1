3453ed7b61014790de2ca7eb0e7af5cb55af72a1
richard <richard@richard-Inspiron-5523.(none)>
2013-06-30 19:08:08 +0100

    small routes tidy up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,18 +3,9 @@ class PagesController < ApplicationController
 include PhotoGrabber::PhotoRetriever
  
 def home
-
-@latest = Portfolio.last_three_photos
+  @latest = Portfolio.last_three_photos
 end
 
-def about
-
-end
-
-  
-def services
-  
-end
 
 def portfolio
   @portfolio = Portfolio.portfolio_photos
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Practice1::Application.routes.draw do
   root :to => 'pages#home' 
 
   match '/home', to: 'pages#home'
-  match '/about', to: 'pages#about'
-  match '/services', to: 'pages#services'
   match '/portfolio', to: 'pages#portfolio'
   match '/contacts', to: 'contacts#new'
 
