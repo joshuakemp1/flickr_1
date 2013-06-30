a8d9546213353b2dfad9ce4e54de34959244f8fb
richard <richard@richard-Inspiron-5523.(none)>
2013-06-30 21:16:06 +0100

    mailer config for produciton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
   config.action_mailer.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: 587,
-  domain: "gmail.com",
+  domain: "heroku.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV['SENDGRID_USERNAME'],
